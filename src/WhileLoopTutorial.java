ca89064d0eb6f324a6f8cb07c173c04cb9707fd9
saiif12 <saif_khan09@hotmail.com>
2021-01-24 00:55:10 +0500

    part2

---
+++ src/WhileLoopTutorial.java
@@ -0,0 +1,14 @@
+public class WhileLoopTutorial {
+
+    public static void main(String[] args) {
+        boolean Loop = true;
+
+        int number = 23;
+
+        while(number<34){
+            System.out.println("Okay" + " " + number);
+
+            number = number + 2;
+        }
+    }
+}
