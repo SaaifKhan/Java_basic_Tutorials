acc57bd21eed5814bbe2a33c2b1889c1585182f0
saiif12 <saif_khan09@hotmail.com>
2021-01-23 21:18:25 +0500

    Initial commit

---
+++ src/VaribaleTutorial.java
@@ -0,0 +1,23 @@
+public class VaribaleTutorial {
+    public static void main(String[] args) {
+        int aValue;
+        aValue = 50;
+        short aShort = 100;
+        long aLong = 1024;
+        double aDouble = 3.4559595;
+        float aFloat = 5.23344f;
+
+
+        char aChar = 'g';
+        boolean aBoolean = false;
+        byte aByte = 123;
+        System.out.println(aValue);
+        System.out.println(aChar);
+        System.out.println(aDouble);
+        System.out.println(aFloat);
+        System.out.println(aShort);
+
+
+        //so this is some Primitive data types and variable intizalsation and calling of any Variables in java//
+    }
+}
