4bd17c0e9629a7f7d0d88bbb7d0395d2cf664760
saiif12 <saif_khan09@hotmail.com>
2021-01-23 22:12:39 +0500

    String

--- src/String.java
+++ src/String.java
@@ -2,6 +2,14 @@ public class String {
     public static void main(String[] args) {
 
 
-        int a
+        int aNumber = 9;
+        java.lang.String text = "hi";
+        java.lang.String SecondText = "there";
+        java.lang.String Blank = "";
+        java.lang.String Blank2 = text + SecondText;
+
+        System.out.println(text);
+        System.out.println(text + Blank + SecondText);
+        System.out.println(Blank2);
     }
 }
